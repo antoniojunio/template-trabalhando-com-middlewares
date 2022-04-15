1f5dd3a4f85cd8720bcd11ee54e9375c26a4726e
antonio_juniorsi <antonio.sa@compasso.com.br>
2022-04-15 13:05:03 -0300

    feat: implemented challenge with middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) {
+    return next();
+  } else if (!user.pro && user.todos.lenght + 1 <= 10) {
+    return next();
+  }
+
+  return response.status(403).json({ error: "You already have ten todos created. Please, change to Pro Plan!" });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id: todoId } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User don't found" });
+  }
+
+  if (!validate(todoId)) {
+    return response.status(400).json({ error: "Todo ID is incorrect." });
+  }
+
+  const userTodo = user.todos.find(todo => todo.id === todoId);
+
+  if (!userTodo) {
+    return response.status(404).json({ error: "User don't found" });
+  }
+
+  request.todo = userTodo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User don't found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -31,7 +81,7 @@ app.post('/users', (request, response) => {
   const usernameAlreadyExists = users.some((user) => user.username === username);
 
   if (usernameAlreadyExists) {
-    return response.status(400).json({ error: 'Username already exists' });
+    return response.status(400).json({ error: "Username already exists" });
   }
 
   const user = {
@@ -57,7 +107,7 @@ app.patch('/users/:id/pro', findUserById, (request, response) => {
   const { user } = request;
 
   if (user.pro) {
-    return response.status(400).json({ error: 'Pro plan is already activated.' });
+    return response.status(400).json({ error: "Pro plan is already activated." });
   }
 
   user.pro = true;
@@ -112,7 +162,7 @@ app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, re
   const todoIndex = user.todos.indexOf(todo);
 
   if (todoIndex === -1) {
-    return response.status(404).json({ error: 'Todo not found' });
+    return response.status(404).json({ error: "User don't found" });
   }
 
   user.todos.splice(todoIndex, 1);
